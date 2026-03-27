eb651b321a79b102cb0f0fa29d0bfa0c1188c4d2
Jens Bösche <pinklion@mac.speedport.ip>
2026-03-27 17:58:58 +0100

    did some more collision

--- src/game/scenes/MainCity.js
+++ src/game/scenes/MainCity.js
@@ -83,12 +83,25 @@ export class MainCity extends Phaser.Scene {
         const buildingParts = [
             // Roof Top Row
             { id: 'redRoofEdgeLeftTop', tiles: [2752, 2784, 2816, 2848, 2880, 2912, 2944, 2976, 3008, 3040], width: 1, collisions: [{ "x": 0, "y": 0, "width": 16, "height": 159, "isSensor": false }] },
-            { id: 'redRoofEdgeMiddleTop', tiles: [2753, 2754, 2755, 2756, 2757, 2785, 2786, 2787, 2788, 2789, 2817, 2818, 2819, 2820, 2821, 2849, 2850, 2851, 2852, 2853, 2881, 2882, 2883, 2884, 2885, 2913, 2914, 2915, 2916, 2917, 2945, 2946, 2947, 2948, 2949, 2977, 2978, 2979, 2980, 2981, 3009, 3010, 3011, 3012, 3013, 3041, 3042, 3043, 3044, 3045], width: 5 },
-            { id: 'redRoofEdgeRightTop', tiles: [2752, 2784, 2816, 2848, 2880, 2912, 2944, 2976, 3008, 3040], flipX: true, width: 1 },
+            {
+                id: 'redRoofEdgeMiddleTop', tiles: [2753, 2754, 2755, 2756, 2757, 2785, 2786, 2787, 2788, 2789, 2817, 2818, 2819, 2820, 2821, 2849, 2850, 2851, 2852, 2853, 2881, 2882, 2883, 2884, 2885, 2913, 2914, 2915, 2916, 2917, 2945, 2946, 2947, 2948, 2949, 2977, 2978, 2979, 2980, 2981, 3009, 3010, 3011, 3012, 3013, 3041, 3042, 3043, 3044, 3045], width: 5, collisions:
+                    [{ "x": 0, "y": 0, "width": 79, "height": 18, "isSensor": false },
+                    { "x": 0, "y": 94, "width": 81, "height": 64, "isSensor": false }]
+            },
+            { id: 'redRoofEdgeRightTop', tiles: [2752, 2784, 2816, 2848, 2880, 2912, 2944, 2976, 3008, 3040], flipX: true, width: 1, collisions: [{ "x": 0, "y": 0, "width": 16, "height": 159, "isSensor": false }] },
             // Middle Building Row
-            { id: 'redBuildingMiddleEdgeLeft', tiles: [3104, 3136, 3168], width: 1 },
-            { id: 'redBuildingMiddleEdgeMiddle', tiles: [3105, 3106, 3107, 3108, 3109, 3137, 3138, 3139, 3140, 3141, 3169, 3170, 3171, 3172, 3173], width: 5 },
-            { id: 'redBuildingMiddleEdgeRight', tiles: [3104, 3136, 3168], flipX: true, width: 1 },
+            {
+                id: 'redBuildingMiddleEdgeLeft', tiles: [3104, 3136, 3168], width: 1,
+                collisions: [{ "x": 0, "y": 0, "width": 16, "height": 50, "isSensor": false }]
+            },
+            {
+                id: 'redBuildingMiddleEdgeMiddle', tiles: [3105, 3106, 3107, 3108, 3109, 3137, 3138, 3139, 3140, 3141, 3169, 3170, 3171, 3172, 3173], width: 5,
+                collisions: [{ "x": 0, "y": 0, "width": 82, "height": 49, "isSensor": false }]
+            },
+            {
+                id: 'redBuildingMiddleEdgeRight', tiles: [3104, 3136, 3168], flipX: true, width: 1,
+                collisions: [{ "x": 0, "y": 0, "width": 16, "height": 50, "isSensor": false }]
+            },
             // Ground Building Row
             {
                 id: 'redBuildingGroundEdgeLeft', tiles: [3232, 3264, 3296, 3328], width: 1, collisions: [
